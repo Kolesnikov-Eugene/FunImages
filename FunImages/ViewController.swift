fd463fb1d4777085fcb33d1ad4f8c111d0ca2def
Eugene Kolesnikov <eugene-kolesnikov@hotmail.com>
2023-05-01 20:36:27 +0300

    Initial Commit

---
+++ FunImages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunImages
+//
+//  Created by Eugene Kolesnikov on 01.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
