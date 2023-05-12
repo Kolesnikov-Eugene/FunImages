7d9730215c6d918259406b4c3e4489836b5a505e
Eugene Kolesnikov <eugene-kolesnikov@hotmail.com>
2023-05-12 20:56:31 +0300

    ProfileViewController added. changed status bar color to white

---
+++ FunImages/ImagesList/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  FunImages
+//
+//  Created by Eugene Kolesnikov on 12.05.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var profileNameLabel: UILabel!
+    @IBOutlet weak var profileAccountLabel: UILabel!
+    @IBOutlet weak var profileInfoLabel: UILabel!
+    @IBOutlet weak var logOutButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    @IBAction func logOutButtonPressed(_ sender: UIButton) {
+    }
+    
+}
